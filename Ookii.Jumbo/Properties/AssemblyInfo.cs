df97fa0bcd23ceb52c01af3ec034a239992459a5
Sven Groot <svgroot@corp.microsoft.com>
2019-09-12 10:38:58 -0700

    Sorted out assembly properties.

--- Ookii.Jumbo/Properties/AssemblyInfo.cs
+++ Ookii.Jumbo/Properties/AssemblyInfo.cs
@@ -1,23 +1,3 @@
-﻿// $Id$
-//
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System;
+﻿using System;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
-[assembly: System.Resources.NeutralResourcesLanguage("en-US")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("902ff4fa-a40b-49bc-a1c3-cf4f90ed64bc")]
