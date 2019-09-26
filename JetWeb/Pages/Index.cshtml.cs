26157d7e1fff3325f08f476e56b8fee4fbfa9062
Sven Groot <svgroot@corp.microsoft.com>
2019-09-25 17:15:30 -0700

    Add beginnings of JetWeb.

---
+++ JetWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Ookii.Jumbo.Jet;
+
+namespace JetWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public JetMetrics Metrics { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        public JobStatus[] RunningJobs { get; set; }
+        public JobStatus[] FinishedJobs { get; set; }
+        public JobStatus[] FailedJobs { get; set; }
+
+
+        public void OnGet()
+        {
+            try
+            {
+                JetClient client = new JetClient();
+                Metrics = client.JobServer.GetMetrics();
+                ViewData["Title"] = Metrics.JobServer.ToString();
+
+                RunningJobs = Metrics.RunningJobs.Select(id => client.JobServer.GetJobStatus(id)).ToArray();
+                FinishedJobs = Metrics.FinishedJobs.Select(id => client.JobServer.GetJobStatus(id)).ToArray();
+                FailedJobs = Metrics.FailedJobs.Select(id => client.JobServer.GetJobStatus(id)).ToArray();
+            }
+            catch (SocketException ex)
+            {
+                ErrorMessage = string.Format("Unable to connect to job server at {0}:{1}. Note: if you changed the job server port in jet.config, you must also modify web.config. Error message: {2}", JetConfiguration.GetConfiguration().JobServer.HostName, JetConfiguration.GetConfiguration().JobServer.Port, ex.Message);
+            }
+        }
+    }
+}
