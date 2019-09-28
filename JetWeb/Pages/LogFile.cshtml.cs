240552da63c991995ad39e3e6c3dc6991fda0bf5
Sven Groot <svgroot@corp.microsoft.com>
2019-09-27 17:39:45 -0700

    Added JetWeb LogFile page.

---
+++ JetWeb/Pages/LogFile.cshtml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Ookii.Jumbo;
+using Ookii.Jumbo.Dfs;
+using Ookii.Jumbo.Dfs.FileSystem;
+using Ookii.Jumbo.Jet;
+
+namespace JetWeb.Pages
+{
+    public class LogFileModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)]
+        public string TaskServer { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int Port { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string MaxSize { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string Kind { get; set; }
+
+        [BindProperty(SupportsGet = true)] 
+        public string TaskId { get; set; }
+
+        [BindProperty(SupportsGet = true)] 
+        public Guid JobId { get; set; }
+
+        [BindProperty(SupportsGet = true)] 
+        public int Attempt { get; set; }
+
+        [BindProperty(SupportsGet = true)] 
+        public bool Profile { get; set; }
+
+        public HtmlString LogFileContents { get; set; }
+
+        public void OnGet()
+        {
+            LogFileKind logKind;
+            switch (Kind)
+            {
+            case "out":
+                logKind = LogFileKind.StdOut;
+                break;
+
+            case "err":
+                logKind = LogFileKind.StdErr;
+                break;
+
+            default:
+                logKind = LogFileKind.Log;
+                break;
+            }
+
+            int maxSize = Int32.MaxValue;
+            if (MaxSize != null)
+                maxSize = (int)BinarySize.Parse(MaxSize, CultureInfo.InvariantCulture);
+
+            string log;
+            if (TaskServer == null)
+            {
+                JetClient client = new JetClient();
+                JetMetrics metrics = client.JobServer.GetMetrics();
+                ViewData["Title"] = $"Job server {metrics.JobServer} log file";
+                log = client.JobServer.GetLogFileContents(logKind, maxSize);
+            }
+            else
+            {
+                var client = JetClient.CreateTaskServerClient(new ServerAddress(TaskServer, Port));
+                if (TaskId == null)
+                {
+                    ViewData["Title"] = $"Task server {TaskServer}:{Port} log file";
+                    log = client.GetLogFileContents(logKind, maxSize);
+                }
+                else
+                {
+                    if (Profile)
+                    {
+                        log = client.GetTaskProfileOutput(JobId, new TaskAttemptId(new TaskId(TaskId), Attempt));
+                        ViewData["Title"] = $"Task {JobId:B}_{TaskId}_{Attempt} profile output (on {TaskServer}) - Jumbo Jet";
+                    }
+                    else
+                    {
+                        log = client.GetTaskLogFileContents(JobId, new TaskAttemptId(new TaskId(TaskId), Attempt), maxSize);
+                        ViewData["Title"] = $"Task {JobId:B}_{TaskId}_{Attempt} log file (on {TaskServer}) - Jumbo Jet";
+                    }
+                }
+            }
+
+            LogFileContents = FormatLogFile(log);
+        }
+
+        private static HtmlString FormatLogFile(string log)
+        {
+            StringBuilder result = new StringBuilder(log.Length);
+            using (StringReader reader = new StringReader(log))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.Contains(" WARN ", StringComparison.Ordinal))
+                        result.AppendFormat(CultureInfo.CurrentCulture, "<span class=\"logWarning\">{0}</span>", HttpUtility.HtmlEncode(line));
+                    else if (line.Contains(" ERROR ", StringComparison.Ordinal))
+                        result.AppendFormat(CultureInfo.CurrentCulture, "<span class=\"logError\">{0}</span>", HttpUtility.HtmlEncode(line));
+                    else
+                        result.Append(HttpUtility.HtmlEncode(line));
+
+                    result.AppendLine();
+                }
+            }
+
+            return new HtmlString(result.ToString());
+        }
+    }
+}
