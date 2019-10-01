8200af29a11c64a3fb8fcf09b3d67180c9256d78
Sven Groot <svgroot@corp.microsoft.com>
2019-09-30 17:25:57 -0700

    Added archived jobs page.

---
+++ JetWeb/Pages/Archive.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Ookii.Jumbo.Jet;
+
+namespace JetWeb.Pages
+{
+    public class ArchiveModel : PageModel
+    {
+        public IEnumerable<ArchivedJob> Jobs { get; set; }
+
+        public void OnGet()
+        {
+            var client = new JetClient();
+            Jobs = client.JobServer.GetArchivedJobs().Reverse();
+        }
+    }
+}
