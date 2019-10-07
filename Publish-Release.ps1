81abc4295557e58807460f41092d0960caf9d4bf
Sven Groot <svgroot@corp.microsoft.com>
2019-10-07 16:03:07 -0700

    Don't require absolute path in Publish-Release.ps1

--- Publish-Release.ps1
+++ Publish-Release.ps1
@@ -3,6 +3,7 @@ param(
     [Parameter(Mandatory=$false)][string]$Configuration = "Release"
 )
 
+$OutputPath = [System.IO.Path]::GetFullPath($OutputPath)
 $binPath = (Join-Path $OutputPath "bin")
 dotnet publish -c $Configuration -o $binPath
 Copy-Item (Join-Path $PSScriptRoot "scripts" "*") $OutputPath -Recurse -Force
